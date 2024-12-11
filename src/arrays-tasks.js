f3aef0b62f37e4860420a94b159aa701cdec6c66
Konstantin F <kf4654569@gmail.com>
2024-12-11 23:48:28 +0300

    update

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -493,8 +493,15 @@ function getMaxItems(arr, n) {
  *    findCommonElements(['a', 'b', 'c'], ['b', 'c', 'd']) => [ 'b', 'c' ]
  *    findCommonElements([1, 2, 3], ['a', 'b', 'c']) => []
  */
-function findCommonElements(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function findCommonElements(arr1, arr2) {
+  return arr1
+    .map((elem) => {
+      if (arr2.includes(elem)) {
+        return elem;
+      }
+      return null;
+    })
+    .filter((item) => item !== null);
 }
 
 /**
@@ -526,8 +533,11 @@ function findLongestIncreasingSubsequence(/* nums */) {
  *  propagateItemsByPositionIndex([ 'a', 'b', 'c', null ]) => [ 'a', 'b', 'b', 'c', 'c', 'c',  null, null, null, null ]
  *  propagateItemsByPositionIndex([ 1,2,3,4,5 ]) => [ 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5 ]
  */
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
+function propagateItemsByPositionIndex(arr) {
+  if (!arr.length) {
+    return [];
+  }
+  return arr.map((elem, index) => Array(index + 1).fill(elem)).flat();
 }
 
 /**
